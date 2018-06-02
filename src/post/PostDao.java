053a26e834bfd5d0b0b3d93adf90c1b369b47d2d
JooHyunWoo <hyunwoo4973@naver.con>
2018-06-02 12:45:39 +0900

    중간 커밋

--- src/post/PostDao.java
+++ src/post/PostDao.java
@@ -8,5 +8,6 @@ public interface PostDao {
 	ArrayList<Post> postList(int brd_id);
 	Post postMenu(int post_id);
 	void deletePost(int post_id);
-	void updatePost(vo.Post post_id);
+	void updatePost(Post post_id);
+	void insertPost(Post post);
 }
