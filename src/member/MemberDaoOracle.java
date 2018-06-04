3bae3ff1e553d4547d41f02f512802544774a43c
myeongseonhyeong <jv8f4@naver.com>
2018-06-05 01:18:51 +0900

    no message

--- src/member/MemberDaoOracle.java
+++ src/member/MemberDaoOracle.java
@@ -141,5 +141,35 @@ public class MemberDaoOracle implements MemberDao {
 		Connection con = null;
 		PreparedStatement pstmt=null;
 		
+		
+		try {
+			con = OracleConnection.getConnection();
+			con.setAutoCommit(false);
+			String sqlinsert= "INSERT INTO MEMBERS(MEM_ID,MEM_USERID,MEM_EMAIL,MEM_PASSWORD,MEM_USERNAME,MEM_NICKNAME,MEM_PHONE,MEM_SEX,MEM_REGISTER_DATETIME,MEM_LASTLOGIN_DATETIME,MEM_TREASURER,MEM_PASSION)\r\n" + 
+					"VALUES ((SELECT MAX(MEM_ID)+1 FROM MEMBERS),?,?,?,?,?,?,?,to_date(sysdate,'RR/MM/DD'),to_date(sysdate,'RR/MM/DD'),0,0)";
+			pstmt = con.prepareStatement(sqlinsert);
+			pstmt.setString(1,member.getMem_userId());
+			pstmt.setString(2,member.getMem_email());
+			pstmt.setString(3,member.getMem_password());
+			pstmt.setString(4,member.getMem_userName());
+			pstmt.setString(5,member.getMem_nickName());
+			pstmt.setString(6,member.getMem_phone());
+			pstmt.setInt(7,member.getMem_sex());
+			int commit = pstmt.executeUpdate();
+			if(commit == 1) {
+				con.commit();
+			} else {
+				con.rollback();
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			OracleConnection.close(pstmt, con);
+		}
+			
+			
+		
+		
+		
 	}
 }
