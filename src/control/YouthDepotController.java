bcf6826962a7f4ef51d46eaa6c2c22a5e4f3f469
KITRI-PC\KITRI <hyunwoo4973>
2018-06-04 15:32:26 +0900

    필요없는 dao service controller 제거

--- src/control/YouthDepotController.java
+++
@@ -1,9 +0,0 @@
-package control;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface YouthDepotController {
-	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException;
-}
