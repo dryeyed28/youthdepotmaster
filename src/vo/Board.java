b0925a6c889671874a8c5d4f36a1c08bc17be9f8
SAMSUNG\eunjeong <juna9411@gmail.com>
2018-06-04 15:03:44 +0900

    brd_type 추가

--- src/vo/Board.java
+++ src/vo/Board.java
@@ -3,14 +3,16 @@ package vo;
 public class Board {
 	private int brd_id;
 	private String brd_name;
+	private String brd_type;
 	private int brd_count;
 	public Board() {
 		super();
 	}
-	public Board(int brd_id, String brd_name, int brd_count) {
+	public Board(int brd_id, String brd_name, String brd_type, int brd_count) {
 		super();
 		this.brd_id = brd_id;
 		this.brd_name = brd_name;
+		this.brd_type = brd_type;
 		this.brd_count = brd_count;
 	}
 	public int getBrd_id() {
@@ -25,12 +27,17 @@ public class Board {
 	public void setBrd_name(String brd_name) {
 		this.brd_name = brd_name;
 	}
+	public String getBrd_type() {
+		return brd_type;
+	}
+	public void setBrd_type(String brd_type) {
+		this.brd_type = brd_type;
+	}
 	public int getBrd_count() {
 		return brd_count;
 	}
 	public void setBrd_count(int brd_count) {
 		this.brd_count = brd_count;
 	}
-	
-		
+			
 }
